8c7da9eafb3bb1852dc884a21d64723b9e5d8f2b
Fby <594018064@qq.com>
2018-12-19 23:18:52 +0800

    tarde

--- src/main/java/cn/source/mapper/TardeMapper.java
+++ src/main/java/cn/source/mapper/TardeMapper.java
@@ -3,18 +3,26 @@ package cn.source.mapper;
 import cn.source.pojo.Tarde;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 public interface TardeMapper {
+	//删除
+	@Delete("delete from tarde where tId= #{tid}")
     int deleteByPrimaryKey(Integer tid);
-
+	//添加
+    @Insert("insert into tarde(tName) values(#{tname})")
     int insert(Tarde record);
-
-    int insertSelective(Tarde record);
-
+    //查询所有行业
+  	@Select("select * from tarde")
     List<Tarde> selectByExample();
-
-    Tarde selectByPrimaryKey(Integer tid);
-
+    //修改
+  	@Update("update tarde set tName=#{tname} where tId=#{tid}")
     int updateByPrimaryKeySelective(Tarde record);
 
     int updateByPrimaryKey(Tarde record);
+    int insertSelective(Tarde record);
+    Tarde selectByPrimaryKey(Integer tid);
 }
